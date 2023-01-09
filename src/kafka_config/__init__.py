74e0db3eb2d6b601da7542f3fec7bafc28fa181e
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-01-09 23:32:54 +0530

    done

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -2,20 +2,18 @@
 import os
 
 # Cloud api details
-API_KEY = "5YCGU43L5J4NXDNY" #os.getenv('API_KEY',None)
-API_SECRET_KEY = "MGR3Angl7vVaA7ojFLmf7RlPBQnJGal/gdpP+t4UR6qrr545iKBinuh7HsLIflwc" #os.getenv('API_SECRET_KEY',None)
-BOOTSTRAP_SERVER = "pkc-41p56.asia-south1.gcp.confluent.cloud:9092" #os.getenv('BOOTSTRAP_SERVER',None)
+API_KEY = os.getenv('API_KEY',None)
+API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
+BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
 
 # Authentication related variables
-# SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
-# SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
-SECURITY_PROTOCOL="SASL_SSL"
-SSL_MACHENISM="PLAIN"
+SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
+SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
 
 # Schema related variables
-ENDPOINT_SCHEMA_URL  = "https://psrc-10wgj.ap-southeast-2.aws.confluent.cloud" #os.getenv('ENDPOINT_SCHEMA_URL',None)
-SCHEMA_REGISTRY_API_KEY = "SSSALNXMCVW5JY43" #os.getenv('SCHEMA_REGISTRY_API_KEY',None)
-SCHEMA_REGISTRY_API_SECRET = "re7qwus0P/B2Y8QhbR7t0sm5ZxFfcgIsUHlpkFkMXxu8x5UxgMyQ+yINwlahwSdT" #os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
+ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
+SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
+SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 
 
 def sasl_conf():
@@ -28,7 +26,7 @@ def sasl_conf():
                 'sasl.username': API_KEY,
                 'sasl.password': API_SECRET_KEY
                 }
-    #print(sasl_conf)
+    print(sasl_conf)
     return sasl_conf
 
 
--- src/kafka_producer/json_producer.py
+++ src/kafka_producer/json_producer.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2020 Confluent Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-# A simple example demonstrating use of JSONSerializer.
-
 import argparse
 from uuid import uuid4
 from src.kafka_config import sasl_conf, schema_config
@@ -31,7 +11,7 @@ import pandas as pd
 from typing import List
 from src.entity.generic import Generic, instance_to_dict
 
-file_path="G:\100-days-of-dl\KrishNaik\ml_data_pipeline\kafka_sensor\sample_data\kafka-sensor-topic\aps_failure_training_set1.csv"
+FILE_PATH = "G:\100-days-of-dl\Krish Naik\ml_data_pipeline\kafka_sensor\sample_data\kafka-sensor-topic\aps_failure_training_set1.csv"
 
 
 def car_to_dict(car: Generic, ctx):
@@ -93,5 +73,3 @@ def product_data_using_file(topic,file_path):
     except ValueError:
         print("Invalid input, discarding record...")
         pass
-
-    
